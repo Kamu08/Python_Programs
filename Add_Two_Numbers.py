c001025c183ae8bad8f4d17d13bd5a159860180a
Kamal Sharma <87929852+Kamu08@users.noreply.github.com>
2023-12-07 15:54:35 +0000

    container config done

---
+++ Add_Two_Numbers.py
@@ -0,0 +1,4 @@
+int1 = int(input())
+int2 = int(input())
+Sum = int1 + int2
+print("The sum of given nummber is", Sum)
---
+++ Find_Square_Root.py
@@ -0,0 +1,8 @@
+# num = int(input("Enter the number for square root "))
+# sqare_root = num**(1/2)
+# print("The square root of your number is", sqare_root)
+
+import math
+num = int(input("Enter the number square root "))
+square_root = math.sqrt(num)
+print("The suare root of num is ", square_root)
